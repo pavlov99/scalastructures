1684f05c6f2653e0301681099299052084c8e01e
Kirill Pavlov <kirill.pavlov@phystech.edu>
2016-02-28 18:27:20 +0800

    simplify heap initialization, add linear heapify and companion object to the Heap.

--- src/main/scala/com/github/pavlov99/heap/Heap.scala
+++ src/main/scala/com/github/pavlov99/heap/Heap.scala
@@ -11,19 +11,32 @@ class Heap[T: Ordering](val keys: Vector[T]) {
   */
   def insert(key: T): Heap[T] = new Heap(keys :+ key) siftUp size
   def :+ (key: T) = insert(key)
+
+  /** Extract minimal element according to current ordering.
+   *  @returns (element, new heap)
+   */
   def extract(): (T, Heap[T]) = (keys(0), new Heap(keys.last +: keys.tail.dropRight(1)) siftDown 0)
+
+  /** Build heap from current vecotor of elements.
+   *  Complexity: O(n)
+   */
+  def heapify(): Heap[T] = heapify0(size / 2 to 0 by -1)
+  private def heapify0(indexes: Seq[Int]): Heap[T] = {
+    if (indexes.isEmpty) this
+    else this siftDown indexes.head heapify0 indexes.tail
+  }
   
   /** Swap two elements in vector and return result vector.*/
   private def swap(xs: Vector[T], i: Int, j: Int): Vector[T] = xs updated (i, xs(j)) updated (j, xs(i))
   
   /** Sift keys down. Method operates only on keys.*/
   private def siftDownKeys(keys: Vector[T], i: Int): Vector[T] = {
-    while (2 * i + 1 < keys.size) {
+    while (2 * i + 1 < size) {
       val left = 2 * i + 1  // left child
       val right = left + 1  // right child
       var j = left
       // keys(right) < keys(left)
-      if (right < keys.size && keyOrdering.compare(keys(right), keys(left)) < 0)
+      if (right < size && keyOrdering.compare(keys(right), keys(left)) < 0)
         j = right
       if (keyOrdering.compare(keys(i), keys(j)) <= 0) return keys
       return siftDownKeys(swap(keys, i, j), j)
@@ -53,5 +66,9 @@ class Heap[T: Ordering](val keys: Vector[T]) {
   *  Complexity O(log n)
   *  @return heap with sifted-up element.
   */
-  def siftUp(i: Int): Heap[T] = new Heap(siftUpKeys(keys, i))
+  private def siftUp(i: Int): Heap[T] = new Heap(siftUpKeys(keys, i))
+}
+
+object Heap {
+  def apply[T: Ordering](keys: T*): Heap[T] = new Heap(Vector(keys: _*)) heapify
 }
--- src/test/scala/com/github/pavlov99/heap/HeapSpec.scala
+++ src/test/scala/com/github/pavlov99/heap/HeapSpec.scala
@@ -14,7 +14,40 @@ class HeapSpec extends FlatSpec {
   }
 
   "Not empty heap" should "extract correct minimum" in {
-    val h = new Heap(Vector(11)) :+ 9 :+ 9 :+ 6 :+ 8 :+ 3 :+ 4 :+ 12
-    assert(h.extract._1 == 3)
+    val h = new Heap(Vector(11))
+      .insert(9)
+      .insert(6)
+      .insert(8)
+      .insert(3)
+    val (min1, h1) = h.extract
+    val (min2, h2) = h1.extract
+    val (min3, h3) = h2.extract
+    assert(min1 == 3)
+    assert(min2 == 6)
+    assert(min3 == 8)
+    assert(h.size == 5)
+    assert(h1.size == 4)
+    assert(h2.size == 3)
+  }
+
+  it should "allow simple init" in {
+    assert(Heap().keys == Vector())
+    assert(Heap(1, 2, 3).keys == Vector(1, 2, 3))
+  }
+
+  it should "allow simple insert" in {
+    assert((Heap(1) :+ 2).keys == Vector(1, 2))
+  }
+
+  it should "support custom ordering" in {
+    val ordering = implicitly[Ordering[Int]].reverse
+    val maxHeap = Heap(1, 4, 3, 5, 4, 2)(ordering)
+
+    val (min1, h1) = maxHeap.extract
+    val (min2, h2) = h1.extract
+    val (min3, h3) = h2.extract
+    assert(min1 == 5)
+    assert(min2 == 4)
+    assert(min3 == 4)
   }
 }
