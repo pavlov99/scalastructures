4a27613c2dcf3f39c91d1a5546f5a319f2ddc895
Kirill Pavlov <kirill.pavlov@phystech.edu>
2016-10-09 22:26:21 +0800

    fix scala style

--- src/main/scala/com/github/pavlov99/tree/Treap.scala
+++ src/main/scala/com/github/pavlov99/tree/Treap.scala
@@ -1,5 +1,8 @@
 package com.github.pavlov99
 
+import scala.collection.immutable.Stack
+
+
 abstract sealed class Treap[+K <% Ordered[K], +P <% Ordered[P]]{
   def key: K
   def priority: P
@@ -9,68 +12,96 @@ abstract sealed class Treap[+K <% Ordered[K], +P <% Ordered[P]]{
   def isEmpty: Boolean
 
   override def toString: String =
-    if (isEmpty) "."
-    else "{" + left + "(" + key + ", " + priority + ")" + right + "}"
-  
-  def add[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](k: K1, p: P1): Treap[K1, P1] =
-    if (isEmpty) Treap.make(k, p)
-    else if (k < key) Treap.make(key, priority, left.add(k, p), right)
-    else if (k > key) Treap.make(key, priority, left, right.add(k, p))
-    else throw new RuntimeException("Duplicated key")
-  
+    if (isEmpty) {
+      "."
+    } else {
+      "{" + left + "(" + key + ", " + priority + ")" + right + "}"
+    }
+
   def split[K1 >: K <% Ordered[K1]](k: K1): (Treap[K, P], Treap[K, P]) =
-    if (isEmpty) (Treap.empty[K, P], Treap.empty[K, P])
-    else if (k > key) right.split(k) match {
-      case (t1, t2) => (Treap.make(key, priority, left, t1), t2)
+    if (isEmpty) {
+      (Treap.empty[K, P], Treap.empty[K, P])
+    } else if (k > key) {
+      right.split(k) match {
+        case (t1, t2) => (Treap.make(key, priority, left, t1), t2)
+      }
+    } else {
+      left.split(k) match {
+        case (t1, t2) => (t1, Treap.make(key, priority, t2, right))
+      }
     }
-    else left.split(k) match {
-      case (t1, t2) => (t1, Treap.make(key, priority, t2, right))
-    }
-  
+
   def merge[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](t: Treap[K1, P1]): Treap[K1, P1] =
-    if (t.isEmpty) this
-    else if (isEmpty) t
-    else if (priority > t.priority) Treap.make(key, priority, left, right merge t)
-    else Treap.make(t.key, t.priority, this merge t.left, t.right)
-  
-  def ++ [K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](t: Treap[K1, P1]) = merge(t)
- 
+    if (t.isEmpty) {
+      this
+    } else if (isEmpty) {
+      t
+    } else if (priority > t.priority) {
+      Treap.make(key, priority, left, right merge t)
+    } else {
+      Treap.make(t.key, t.priority, this merge t.left, t.right)
+    }
+
+  // scalastyle:off method.name
+  def ++[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](t: Treap[K1, P1]): Treap[K1, P1] = merge(t)
+  // scalastyle:on
+
   def insert[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](k: K1, p: P1): Treap[K1, P1] =
     split(k) match {
       case(t1, t2) => t1.merge(Treap.make(k, p)).merge(t2)
     }
-  
-  def :+ [K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](k: K1, p: P1) = insert(k, p)
+
+  // scalastyle:off method.name
+  def :+[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](k: K1, p: P1): Treap[K1, P1] = insert(k, p)
+  // scalastyle:on
+
   def head: (K, P) = (key, priority)
   def tail: Treap[K, P] = left merge right
   def extract(): ((K, P), Treap[K, P]) = (head, tail)
-  
+
   def remove[K1 >: K <% Ordered[K1], P1 >: P <% Ordered[P1]](k: K1): Treap[K1, P1] =
-    if (isEmpty) throw new RuntimeException("An empty tree.")
-    else if (k < key) Treap.make(key, priority, left.remove(k), right)
-    else if (k > key) Treap.make(key, priority, left, right.remove(k))
-    else left merge right
-  
+    if (isEmpty) {
+      throw new RuntimeException("An empty tree.")
+    } else if (k < key) {
+      Treap.make(key, priority, left.remove(k), right)
+    } else if (k > key) {
+      Treap.make(key, priority, left, right.remove(k))
+    } else {
+      left merge right
+    }
+
   def contains[K1 >: K <% Ordered[K1]](k: K1): Boolean =
-    if (isEmpty) false
-    else if (k == key) true
-    else if (k < key) left.contains(k)
-    else right.contains(k)
-  
+    if (isEmpty) {
+      false
+    } else if (k == key) {
+      true
+    } else if (k < key) {
+      left.contains(k)
+    } else {
+      right.contains(k)
+    }
+
   def height: Int = if (isEmpty) 0 else 1 + math.max(left.height, right.height)
-  
+
   def min: (K, P) = {
-    def loop(t: Treap[K, P], k: K, p: P): (K, P) = if (t.isEmpty) (k, p) else loop(t.left, t.key, t.priority)
-    if (isEmpty) throw new RuntimeException("An empty tree.")
-    else loop(left, key, priority)
-  }
-  
-  def max: (K, P) = {
-    def loop(t: Treap[K, P], k: K, p: P): (K, P) = if (t.isEmpty) (k, p) else loop(t.right, t.key, t.priority)
-    if (isEmpty) throw new RuntimeException("An empty tree.")
-    else loop(right, key, priority)
+    def loop(t: Treap[K, P], k: K, p: P): (K, P) =
+      if (t.isEmpty) (k, p) else loop(t.left, t.key, t.priority)
+    if (isEmpty) {
+      throw new RuntimeException("An empty tree.")
+    } else {
+      loop(left, key, priority)
+    }
   }
 
+  def max: (K, P) = {
+    def loop(t: Treap[K, P], k: K, p: P): (K, P) =
+      if (t.isEmpty) (k, p) else loop(t.right, t.key, t.priority)
+    if (isEmpty) {
+      throw new RuntimeException("An empty tree.")
+    } else {
+      loop(right, key, priority)
+    }
+  }
 }
 
 case object Leaf extends Treap[Nothing, Nothing] {
@@ -82,19 +113,43 @@ case object Leaf extends Treap[Nothing, Nothing] {
   def isEmpty: Boolean = true
 }
 
-case class Branch[K <% Ordered[K], P <% Ordered[P]](key: K, priority: P, left: Treap[K, P], right: Treap[K, P], size: Int) extends Treap[K, P]{
+case class Branch[K <% Ordered[K], P <% Ordered[P]](
+    key: K,
+    priority: P,
+    left: Treap[K, P],
+    right: Treap[K, P],
+    size: Int) extends Treap[K, P]{
   def isEmpty: Boolean = false
 }
 
 object Treap {
   def empty[K, P]: Treap[K, P] = Leaf
-  def make[K <% Ordered[K], P <% Ordered[P]](k: K, p: P, l: Treap[K, P] = Leaf, r: Treap[K, P] = Leaf): Treap[K, P] = Branch(k, p, l, r, l.size + r.size + 1)
+  def make[K <% Ordered[K], P <% Ordered[P]](
+    k: K,
+    p: P,
+    l: Treap[K, P] = Leaf,
+    r: Treap[K, P] = Leaf): Treap[K, P] = Branch(k, p, l, r, l.size + r.size + 1)
 
   def apply[K <% Ordered[K], P <% Ordered[P]](xs: (K, P)*): Treap[K, P] =
     xs.foldLeft(Treap.empty[K, P])({case(t, (k, p)) => t.insert(k, p)})
-  
-  /** Assumes that keys are ordered. **/
+
   def fromOrdered[K <% Ordered[K], P <% Ordered[P]](xs: (K, P)*): Treap[K, P] = {
-    apply(xs: _*)
+    def loop(k: K, p: P, path: Stack[Treap[K, P]], left: Treap[K, P] = Leaf): Stack[Treap[K, P]] =
+      if (path.isEmpty) {
+        path.push(Treap.make(k, p, left))
+      } else if (p < path.top.priority) {
+        val t = Treap.make(k, p, left)
+        path.pop.push(
+          Treap.make(path.top.key, path.top.priority, path.top.left, t)).push(t)
+      } else {
+        loop(k, p, path.pop, path.top)
+      }
+
+    val path = xs.foldLeft(Stack[Treap[K, P]]())({case(s, (k, p)) => loop(k, p, s)})
+    if (path.isEmpty) {
+      Treap.empty[K, P]
+    } else {
+      path.last
+    }
   }
 }
